ba690b8b258f8a2365ac4a118c4312b07a254a77
Bablu Banik <bablu258@outlook.com>
2022-04-25 16:32:54 -0500

    Test

--- cst438_register/src/main/java/com/cst438/Cst4380wRegistrationApplication.java
+++ cst438_register/src/main/java/com/cst438/Cst4380wRegistrationApplication.java
@@ -11,6 +11,7 @@ import com.cst438.service.GradebookServiceREST;
 @SpringBootApplication
 public class Cst4380wRegistrationApplication  {  
 
+	// minor
 	public static void main(String[] args) {
 		SpringApplication.run(Cst4380wRegistrationApplication.class, args);
 	}
